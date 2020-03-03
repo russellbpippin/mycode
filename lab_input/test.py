040e1be369daf21de2c3dd2178c1768732c9d87f
russellbpippin <russell.pippin@charter.com>
2020-03-03 11:33:51 -0500

    copying local files with python

---
+++ lab_input/test.py
@@ -0,0 +1,6 @@
+#!/user/bin/env python3
+  
+name= input("Please define the user's name:")
+day_of_week= input("what day of th eweek is it:")
+
+print("Hello, " + name + "! Happy " + day_of_week + "!")
